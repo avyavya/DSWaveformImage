4f42972543fee338e4bc22da1f6f65f8132e23dd
avyavya <342615+avyavya@users.noreply.github.com>
2020-02-12 16:19:19 +0900

    Update ViewController.swift

--- DSWaveformImageExample/ViewController.swift
+++ DSWaveformImageExample/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var topWaveformView: UIImageView!
     @IBOutlet weak var middleWaveformView: WaveformImageView!
     @IBOutlet weak var bottomWaveformView: UIImageView!
-
+    @IBOutlet weak var scrollWaveformView: UIScrollView!
+    @IBOutlet weak var scrollWaveformViewContainer: UIView!
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
@@ -50,6 +52,31 @@ class ViewController: UIViewController {
         waveformAnalyzer?.samples(count: 10) { samples in
             print("sampled down to 10, results are \(samples ?? [])")
         }
+
+        // over 5460pt wide waveform image w/ scroll view
+        let scrollContentSize = CGSize(width: 6000, height: scrollWaveformView.frame.height)
+        let scrollViewConfig = WaveformConfiguration(size: scrollContentSize,
+                                                     color: UIColor.darkGray,
+                                                  style: .filled,
+                                                  position: .middle)
+        var scrollViewPosition: CGFloat = 0
+
+        waveformImageDrawer.waveformImage(fromAudioAt: audioURL, with: scrollViewConfig) { [weak self] image in
+            guard let image = image else { return }
+
+            DispatchQueue.main.async {
+                let iv = UIImageView(image: image)
+                iv.frame.origin.x = scrollViewPosition
+                scrollViewPosition += image.size.width
+                self?.scrollWaveformViewContainer.addSubview(iv)
+
+                // set scroll content size
+                guard self?.scrollWaveformViewContainer.subviews.count == 1,
+                    let height = self?.scrollWaveformView.frame.height else { return }
+
+                let sizeFit = CGSize(width: scrollContentSize.width, height: height)
+                self?.scrollWaveformView.contentSize = sizeFit
+            }
+        }
     }
 }
-
